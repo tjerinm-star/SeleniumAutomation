245e654e77d15b3b5a66778f3319b7f6e0aa49d7
tjeri <tjeri@DESKTOP-0GQT428>
2026-02-09 19:39:42 +0530

    9/2/2026

--- GroceryApplicationProject/src/main/java/utilities/ExtentReportUtility.java
+++ GroceryApplicationProject/src/main/java/utilities/ExtentReportUtility.java
@@ -9,15 +9,16 @@ public class ExtentReportUtility
 	 public static final ExtentReports extentReports = new ExtentReports();// static instance of extentreports that can 
 		// be shared accross the application 
 
-public synchronized static ExtentReports createExtentReports() { 
+       public synchronized static ExtentReports createExtentReports()
+       { 
 
-ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html"); 
-reporter.config().setReportName("7RMartSupermarketProject"); 
-extentReports.attachReporter(reporter); 
+          ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html"); 
+          reporter.config().setReportName("7RMartSupermarketProject"); 
+          extentReports.attachReporter(reporter); 
 
-extentReports.setSystemInfo("Organization", "Obsqura"); 
-extentReports.setSystemInfo("Name", " Sona"); // provides context of the report 
-return extentReports; 
+          extentReports.setSystemInfo("Organization", "Obsqura"); 
+          extentReports.setSystemInfo("Name", "Jerin"); // provides context of the report 
+          return extentReports; 
 
 }
 }
